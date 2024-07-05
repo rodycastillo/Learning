3ef9f9f1a9ae1490d3f600ce35a20006bbd294d5
RodyCarlo <rody.castillo.tacuri@gmail.com>
2024-07-04 23:55:55 -0500

    chore[app]: general advances

--- TAILWINDCSS/TEST/tailwind.config.js
+++ TAILWINDCSS/TEST/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'azul-claro': '#243cff',
+        'azul-oscuro': '#0d1b3e'
+      },
+      width: {
+        '42': '170px',
+      },
+      screens: {
+        'tablet': '900px'
+      }
+    },
   },
   plugins: [],
 }
